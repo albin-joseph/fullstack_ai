61ad00a9881d5c2f91f4a7cd0b1b78e886ee7d59
albin-joseph <albinjoseph007@gmail.com>
2026-03-03 11:34:56 +0530

    update module exerciswe

---
+++ 1_python/exercise/exercise7.py
@@ -0,0 +1,8 @@
+import math_operations
+
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+print("Sum: ", math_operations.add(first_number, second_number))
+print("Difference: ", math_operations.subtract(first_number, second_number))
+print("Product: ", math_operations.multiply(first_number, second_number))
+print("Quotient: ", math_operations.divide(first_number, second_number))
