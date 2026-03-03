c207c9cc40a56463bbb61b6bed091f26427339d2
albin-joseph <albinjoseph007@gmail.com>
2026-03-03 12:25:23 +0530

    update list program

--- 1_python/8_program.py
+++ 1_python/8_program.py
@@ -1,8 +1,16 @@
 numbers = [1, 2, 3, 4, 5]
+numbers.append(6)  # Adding an element to the list
 
 fruits = ["apple", "banana", "cherry"]
+fruits.append("date")  # Adding an element to the list
 
 mixed = [1, "apple", 3.14, True]
+mixed.append("new element")  # Adding an element to the list
+
+mixed.remove(3.14)  # Removing an element from the list
+
+numbers[0] = 10  # Modifying an element in the list
+fruits[1] = "blueberry"  # Modifying an element in the list
 
 print(numbers)
 print(fruits)
