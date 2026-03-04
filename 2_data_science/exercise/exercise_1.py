52de4fa8c698f0037a4164be691cb1ddfea513c0
albin-joseph <albinjoseph007@gmail.com>
2026-03-04 11:48:04 +0530

    update operation exercise

---
+++ 2_data_science/exercise/exercise_1.py
@@ -0,0 +1,11 @@
+import numpy as np
+arra_1st = np.array([1,2,3,4,5,6])
+arra_2d = np.array([3,4,6,8,5,6])
+
+print(arra_1st)
+print(arra_2d)
+
+print("Adddition of 2 arrays: ", arra_1st + arra_2d)
+print("Subtraction of 2 arrays: ", arra_1st - arra_2d)
+print("Multiplication of 2 arrays: ", arra_1st * arra_2d)
+print("Division of 2 arrays: ", arra_1st / arra_2d)
