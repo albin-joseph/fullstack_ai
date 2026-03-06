b53e6216642ed74740551f2ce0e92a325c92b71d
albin-joseph <albinjoseph007@gmail.com>
2026-03-06 12:10:37 +0530

    aggregate functions

--- 2_data_science/4_program.py
+++ 2_data_science/4_program.py
@@ -1,10 +1,9 @@
 import numpy as np
 
-#Array as scalar broadcasting
+# Array as scalar broadcasting
 array = np.array([1, 2, 3])
 scalar = 10
-result = array + scalar  # Broadcasting the scalar to each element of the array
-print("Result of adding scalar to array:", result)
+result = array + scalar  # Broadcasting the scalar to each element of the array    print("Result of adding scalar to array:", result)
 
 matrix = np.array([[1, 2], [3, 4]])
 scalar = 5
@@ -15,3 +14,13 @@ vector = np.array([1, 2, 3])
 matrix = np.array([[1, 2, 3], [4, 5, 6]])
 result_vector_matrix = vector + matrix  # Broadcasting the vector to each row of the matrix
 print("Result of adding vector to matrix:\n", result_vector_matrix)
+
+ # Aggregation functions
+array = np.array([1, 2, 3, 4, 5])
+print("Sum of array:", np.sum(array))
+print("Mean of array:", np.mean(array))
+print("Standard deviation of array:", np.std(array))
+matrix = np.array([[1, 2], [3, 4]])
+print("Sum of matrix:", np.sum(matrix))
+print("Mean of matrix:", np.mean(matrix))
+print("Standard deviation of matrix:", np.std(matrix))
